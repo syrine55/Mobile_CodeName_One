12155ea08144f3962367d91595baafb83d8173c0
chekir walid <78790264+walid009@users.noreply.github.com>
2021-05-14 21:25:28 +0100

    Version_final_mobile
    Version_final_mobile

---
+++ WorkShops-CodenameOne-master/src/com/mycompany/gui/GestionCategorieForm.java
@@ -0,0 +1,262 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gui;
+
+import com.codename1.components.ImageViewer;
+import com.codename1.components.ScaleImageLabel;
+import com.codename1.components.SpanLabel;
+import com.codename1.components.ToastBar;
+import com.codename1.ui.Button;
+import com.codename1.ui.ButtonGroup;
+import com.codename1.ui.Command;
+import com.codename1.ui.Component;
+import static com.codename1.ui.Component.BOTTOM;
+import static com.codename1.ui.Component.CENTER;
+import static com.codename1.ui.Component.LEFT;
+import static com.codename1.ui.Component.RIGHT;
+import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Display;
+import com.codename1.ui.EncodedImage;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Graphics;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.RadioButton;
+import com.codename1.ui.Tabs;
+import com.codename1.ui.TextArea;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.URLImage;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.layouts.GridLayout;
+import com.codename1.ui.layouts.LayeredLayout;
+import com.codename1.ui.plaf.Style;
+import com.codename1.ui.util.Resources;
+import com.mycompany.myapp.services.ServiceCategorie;
+import com.mycompany.myapp.services.ServiceMeditation;
+import entity.Categorie;
+import entity.Meditation;
+
+/**
+ *
+ * @author Mega Pc
+ */
+public class GestionCategorieForm extends BaseForm{
+    Form current;
+  public GestionCategorieForm(Resources res) {
+        super("Méditation", BoxLayout.y());
+        Toolbar tb = new Toolbar(true);
+        setToolbar(tb);
+        getTitleArea().setUIID("Container");
+        setTitle("Gestion Catégorie");
+        getContentPane().setScrollVisible(false);
+
+        
+        current=this;
+        
+        super.addSideMenu(res);
+        tb.addSearchCommand(e -> {
+        });
+
+        Tabs swipe = new Tabs();
+
+        Label spacer1 = new Label();
+        Label spacer2 = new Label();
+        addTab(swipe, res.getImage("med.jpg"), spacer1, "", "", "Bienvenue dans nos espace de méditation.");
+        //addTab(swipe, res.getImage("dog.jpg"), spacer2, "100 Likes  ", "66 Comments", "Dogs are cute: story at 11");
+
+        swipe.setUIID("Container");
+        swipe.getContentPane().setUIID("Container");
+        swipe.hideTabs();
+
+        ButtonGroup bg = new ButtonGroup();
+        int size = Display.getInstance().convertToPixels(1);
+        Image unselectedWalkthru = Image.createImage(size, size, 0);
+        Graphics g = unselectedWalkthru.getGraphics();
+        g.setColor(0xffffff);
+        g.setAlpha(100);
+        g.setAntiAliased(true);
+        g.fillArc(0, 0, size, size, 0, 360);
+        Image selectedWalkthru = Image.createImage(size, size, 0);
+        g = selectedWalkthru.getGraphics();
+        g.setColor(0xffffff);
+        g.setAntiAliased(true);
+        g.fillArc(0, 0, size, size, 0, 360);
+        RadioButton[] rbs = new RadioButton[swipe.getTabCount()];
+        FlowLayout flow = new FlowLayout(CENTER);
+        flow.setValign(BOTTOM);
+        Container radioContainer = new Container(flow);
+        for (int iter = 0; iter < rbs.length; iter++) {
+            rbs[iter] = RadioButton.createToggle(unselectedWalkthru, bg);
+            rbs[iter].setPressedIcon(selectedWalkthru);
+            rbs[iter].setUIID("Label");
+            radioContainer.add(rbs[iter]);
+        }
+
+        rbs[0].setSelected(true);
+        swipe.addSelectionListener((i, ii) -> {
+            if (!rbs[ii].isSelected()) {
+                rbs[ii].setSelected(true);
+            }
+        });
+
+        Component.setSameSize(radioContainer, spacer1, spacer2);
+        add(LayeredLayout.encloseIn(swipe, radioContainer));
+
+        ButtonGroup barGroup = new ButtonGroup();
+        RadioButton Categorie = RadioButton.createToggle("Categorie", barGroup);
+        Categorie.setUIID("SelectBar");
+        RadioButton addCat = RadioButton.createToggle("Ajouter", barGroup);
+        addCat.setUIID("SelectBar");
+        RadioButton chart = RadioButton.createToggle("Chart", barGroup);
+        chart.setUIID("SelectBar");
+        Label arrow = new Label(res.getImage("news-tab-down-arrow.png"), "Container");
+
+        add(LayeredLayout.encloseIn(
+                GridLayout.encloseIn(4, Categorie, addCat,chart),
+                FlowLayout.encloseBottom(arrow)
+        ));
+
+        Categorie.setSelected(true);
+        arrow.setVisible(false);
+        addShowListener(e -> {
+            arrow.setVisible(true);
+            updateArrowPosition(Categorie, arrow);
+        });
+        bindButtonSelection(Categorie, arrow);
+        bindButtonSelection(addCat, arrow);
+        
+       
+        Categorie.addActionListener(e -> new GestionCategorieForm(res).show());
+        addCat.addActionListener(e -> new AddCategorieForm(res).show());
+        chart.addActionListener(e -> new ChartCategorieParnbrMed(current).show());
+
+        for (Categorie c : ServiceCategorie.getInstance().getAllCategories()) {
+            add(addItem(c,res));
+        }
+
+        // special case for rotation
+        addOrientationListener(e -> {
+            updateArrowPosition(barGroup.getRadioButton(barGroup.getSelectedIndex()), arrow);
+        });
+
+
+    }
+  //
+  public Container addItem(Categorie c,Resources res) {
+        Label tfName = new Label(c.getNom(), "Nom categorie");
+        Button btnsup = new Button("Supprimer");
+        Button btnmodifier = new Button("Modifier");
+        Container c1 = new Container(BoxLayout.x());
+
+        c1.addAll(tfName, btnsup,btnmodifier);
+
+        btnsup.addActionListener(e->{
+                Categorie cat = new Categorie(c.getId(), tfName.getText());
+                if (new ServiceCategorie().daleteCategorie(cat)) {
+                    Dialog.show("Succcess", "Categorie "+c.getNom()+" supprimer", new Command("ok"));
+                    new GestionCategorieForm(res).show();
+                } else {
+                    Dialog.show("Error", "Server error", new Command("ok"));
+                }
+        });
+        btnmodifier.addActionListener(e->{
+            new UpdateCategorieForm(res,c.getNom(),c.getId()).show();
+               // new UpdateCategorie(pervious,c.getNom(),c.getId()).show();
+        });
+        return c1;
+    }
+  //
+    private void updateArrowPosition(Button b, Label arrow) {
+        arrow.getUnselectedStyle().setMargin(LEFT, b.getX() + b.getWidth() / 2 - arrow.getWidth() / 2);
+        arrow.getParent().repaint();
+        
+        
+    }
+    
+    private void addTab(Tabs swipe, Image img, Label spacer, String likesStr, String commentsStr, String text) {
+        int size = Math.min(Display.getInstance().getDisplayWidth(), Display.getInstance().getDisplayHeight());
+        if(img.getHeight() < size) {
+            img = img.scaledHeight(size);
+        }
+        Label likes = new Label(likesStr);
+        Style heartStyle = new Style(likes.getUnselectedStyle());
+        heartStyle.setFgColor(0xff2d55);
+        FontImage heartImage = FontImage.createMaterial(FontImage.MATERIAL_FAVORITE, heartStyle);
+        likes.setIcon(heartImage);
+        likes.setTextPosition(RIGHT);
+
+        Label comments = new Label(commentsStr);
+        FontImage.setMaterialIcon(comments, FontImage.MATERIAL_CHAT);
+        if(img.getHeight() > Display.getInstance().getDisplayHeight() / 2) {
+            img = img.scaledHeight(Display.getInstance().getDisplayHeight() / 2);
+        }
+        ScaleImageLabel image = new ScaleImageLabel(img);
+        image.setUIID("Container");
+        image.setBackgroundType(Style.BACKGROUND_IMAGE_SCALED_FILL);
+        Label overlay = new Label(" ", "ImageOverlay");
+        
+        Container page1 = 
+            LayeredLayout.encloseIn(
+                image,
+                overlay,
+                BorderLayout.south(
+                    BoxLayout.encloseY(
+                            new SpanLabel(text, "LargeWhiteText"),
+                            FlowLayout.encloseIn(likes, comments),
+                            spacer
+                        )
+                )
+            );
+
+        swipe.addTab("", page1);
+    }
+    
+   private void addButton(Image img, String title, boolean liked, int likeCount, int commentCount) {
+       int height = Display.getInstance().convertToPixels(11.5f);
+       int width = Display.getInstance().convertToPixels(14f);
+       Button image = new Button(img.fill(width, height));
+       image.setUIID("Label");
+       Container cnt = BorderLayout.west(image);
+       cnt.setLeadComponent(image);
+       TextArea ta = new TextArea(title);
+       ta.setUIID("NewsTopLine");
+       ta.setEditable(false);
+
+       Label likes = new Label(likeCount + " Likes  ", "NewsBottomLine");
+       likes.setTextPosition(RIGHT);
+       if(!liked) {
+           FontImage.setMaterialIcon(likes, FontImage.MATERIAL_FAVORITE);
+       } else {
+           Style s = new Style(likes.getUnselectedStyle());
+           s.setFgColor(0xff2d55);
+           FontImage heartImage = FontImage.createMaterial(FontImage.MATERIAL_FAVORITE, s);
+           likes.setIcon(heartImage);
+       }
+       Label comments = new Label(commentCount + " Comments", "NewsBottomLine");
+       FontImage.setMaterialIcon(likes, FontImage.MATERIAL_CHAT);
+       
+       
+       cnt.add(BorderLayout.CENTER, 
+               BoxLayout.encloseY(
+                       ta,
+                       BoxLayout.encloseX(likes, comments)
+               ));
+       add(cnt);
+       image.addActionListener(e -> ToastBar.showMessage(title, FontImage.MATERIAL_INFO));
+   }
+    
+    private void bindButtonSelection(Button b, Label arrow) {
+        b.addActionListener(e -> {
+            if(b.isSelected()) {
+                updateArrowPosition(b, arrow);
+            }
+        });
+    }
+}
