7d2f928980a3efe0a370ddf712b0f49d3008e543
FediKhatib <fedi.khatib@esprit.tn>
2021-05-19 17:40:11 +0100

    f

---
+++ WorkShops-CodenameOne-master/src/com/mycompany/gui/EventChart.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the edit
+ */
+package com.mycompany.gui;
+
+import com.codename1.charts.ChartComponent;
+import com.codename1.charts.models.CategorySeries;
+import com.codename1.charts.renderers.DefaultRenderer;
+import com.codename1.charts.renderers.SimpleSeriesRenderer;
+import com.codename1.charts.util.ColorUtil;
+import com.codename1.charts.views.PieChart;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.layouts.BorderLayout;
+import com.mycompany.myapp.services.ServiceEvent;
+import com.mycompany.myapp.services.ServiceMeditation;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mega Pc
+ */
+public class EventChart extends Form{
+	 int nbre=ServiceEvent.nbrEnligne;
+     int nbrs=ServiceEvent.nbrSalle;
+   
+    public EventChart(Form previous) {
+    	  double[] values = new double[2];
+    		values[0]=nbre;
+    		values[1]=nbrs;
+        getStyle().setBgColor(0x2D2D2D);
+        // Generate the values
+       
+        //ArrayList<Long> med=ServiceMeditation.getInstance().getAllnbEspaseParCategorie();
+          
+        // Set up the renderer
+        int[] colors = new int[]{ColorUtil.BLUE, ColorUtil.GREEN};
+        DefaultRenderer renderer = buildCategoryRenderer(colors);
+        renderer.setZoomButtonsVisible(true);
+        renderer.setZoomEnabled(true);
+        renderer.setChartTitleTextSize(20);
+        renderer.setDisplayValues(true);
+        renderer.setShowLabels(true);
+        SimpleSeriesRenderer r = renderer.getSeriesRendererAt(0);
+        r.setGradientEnabled(true);
+        r.setGradientStart(0, ColorUtil.BLUE);
+        r.setGradientStop(0, ColorUtil.GREEN);
+        r.setHighlighted(true);
+
+        // Create the chart ... pass the values and renderer to the chart object.
+        PieChart chart = new PieChart(buildCategoryDataset("Project budget", values), renderer);
+
+        // Wrap the chart in a Component so we can add it to a form
+        ChartComponent c = new ChartComponent(chart);
+
+        // Create a form and show it.
+        setTitle("Nbr espace par catégorie");
+        setLayout(new BorderLayout());
+        add(BorderLayout.CENTER, c);
+    
+        getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK, e->previous.showBack());
+    }
+
+    /**
+     * Creates a renderer for the specified colors.
+     */
+    private DefaultRenderer buildCategoryRenderer(int[] colors) {
+        DefaultRenderer renderer = new DefaultRenderer();
+        renderer.setLabelsTextSize(50);
+        renderer.setLegendTextSize(50);
+        renderer.setMargins(new int[]{20, 30, 15, 0});
+        for (int color : colors) {
+            SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+            r.setColor(color);
+            renderer.addSeriesRenderer(r);
+        }
+        return renderer;
+    }
+
+    /**
+     * Builds a category series using the provided values.
+     *
+     * @param titles the series titles
+     * @param values the values
+     * @return the category series
+     */
+    protected CategorySeries buildCategoryDataset(String title, double[] values) {
+        CategorySeries series = new CategorySeries(title);
+        int k = 0;
+        
+            series.add("salle", nbrs);
+            series.add("en ligne", nbre);
+       
+        
+        return series;
+    }
+    
+    
+}
---
+++ WorkShops-CodenameOne-master/src/com/mycompany/gui/HomeEvent.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gui;
+
+import com.codename1.ui.Button;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.layouts.BoxLayout;
+
+public class HomeEvent extends Form {
+	String role = "cl";
+
+	Form current;
+	/*
+	 *  tra�e de la Form en cours pour la passer en param�tres aux interfaces
+	 * suivantes pour pouvoir y revenir plus tard en utilisant la m�thode showBack
+	 */
+
+	public HomeEvent() {
+		current = this; // R�cup�ration de l'interface(Form) en cours
+		setTitle("Home");
+		setLayout(BoxLayout.y());
+
+		add(new Label("Choose an option"));
+		Button btnAddTask = new Button("Add Event");
+		Button btnListTasks = new Button("List Events");
+		Button btnParticiper = new Button("Participer");
+		Button btnUserEvent = new Button("Mes Event");
+		if (role.equals("spec")) {
+			btnAddTask.addActionListener(e -> new AddEventForm(current).show());
+			btnListTasks.addActionListener(e -> new ListEvent(current).show());
+			addAll(btnAddTask, btnListTasks);
+
+		} else {
+			btnParticiper.addActionListener(e -> new ListUserEvent(current).show());
+			btnUserEvent.addActionListener(e -> new MesEventLoader(current).show());
+			addAll(btnParticiper, btnUserEvent);
+
+		}
+
+	}
+
+}
